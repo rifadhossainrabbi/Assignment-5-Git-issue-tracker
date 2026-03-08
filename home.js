79e09c70b9e590bba8dd781ce82ca76a29632f0c
rifadhossainrabbi <hossainmrrifad@gmail.com>
2026-03-08 13:39:07 +0600

    Added modal successfully

--- home.js
+++ home.js
@@ -17,6 +17,108 @@ const hideSpinner = () => {
 }
 
 
+// display modal
+const loadModal = (id) => {
+  console.log(id);
+  fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+    .then(res => res.json())
+    .then(data => displayModal(data.data));
+  const modal = document.getElementById("my_modal_1");
+  modal.showModal();
+}
+
+// "id": 1,
+//  "title": "Fix navigation menu on mobile devices",
+//  "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
+//  "status": "open",
+//   "labels": [
+//           "bug",
+//           "help wanted"
+//         ],
+//   "priority": "high",
+//   "author": "john_doe",
+//   "assignee": "jane_smith",
+//   "createdAt": "2024-01-15T10:30:00Z",
+//   "updatedAt": "2024-01-15T10:30:00Z"
+
+const displayModal = (item) => {
+  console.log(item);
+
+  let priorityDesign = "";
+  if (item.priority == "high") {
+    priorityDesign = "bg-red-100 text-red-500 border-none rounded-full text-[14px]";
+  } else if (item.priority == "medium") {
+    priorityDesign = "bg-amber-100 text-amber-500 border-none rounded-full text-[14px]";
+  } else {
+    priorityDesign = "bg-gray-100 text-gray-500 border-none rounded-full text-[14px]"
+  }
+
+  let btns = "";
+  item.labels.forEach(label => {
+    let design = "";
+    let icon = "";
+
+    if (label === "bug") {
+      design = "bg-red-50 text-red-500 border-none rounded-full text-[12px]";
+      icon = "./assets/bug.png";
+    } else if (label === "help wanted") {
+      design = "bg-amber-50 text-amber-500 border-none rounded-full text-[12px]";
+      icon = "./assets/helpWanted.png";
+    } else if (label === "enhancement") {
+      design = "bg-green-50 text-green-500 border-none rounded-full text-[12px]";
+      icon = "./assets/inhancement.png";
+    } else if (label === "good first issue") {
+      design = "bg-blue-50 text-blue-500 border-none rounded-full text-[12px]";
+    } else if (label === "documentation") {
+      design = "bg-amber-50 text-amber-500 border-none rounded-full text-[12px]";
+    }
+
+    // btns += `<button class="btn ${design}"><img src="${icon}" alt=""> ${label}</button>`;
+    btns += `<button class="flex items-center gap-1.5 py-1.5 px-3 ${design}">
+      ${icon ? `<img class="w-[14px] h-[14px] block" src="${icon}" alt="">` : ""} ${label}
+      </button>`;
+  });
+
+  const detailConatiner = document.getElementById("modal_container");
+  detailConatiner.innerHTML = `
+    <h3 class="text-lg font-bold">${item.title}</h3>
+
+        <div class="flex items-center gap-8">
+          <button class="py-1 px-4 bg-green-500 text-white rounded-full">${item.status}</button>
+          <ul class="flex gap-8">
+            <li class="list-disc">Opened by ${item.assignee ? item.assignee : "unknown"}</li>
+            <li class="list-disc">${new Date(item.createdAt).toLocaleDateString()}</li>
+          </ul>
+        </div>
+
+        <!-- buttons -->
+        <div class="flex gap-3">
+            ${btns}
+        </div>
+        <p>${item.description}</p>
+        <div class="bg-gray-200 p-4 grid grid-cols-2">
+
+          <div>
+            <p>Assignee:</p>
+            <h1>${item.assignee ? item.assignee : "unknown"}</h1>
+          </div>
+
+          <div>
+            <p>Priority:</p>
+            <button class="btn ${priorityDesign}">${item.priority}</button>
+          </div>
+        </div>
+
+        <!-- modal close btn -->
+        <div class="modal-action">
+          <form method="dialog">
+            <!-- if there is a button in form, it will close the modal -->
+            <button class="btn">Close</button>
+          </form>
+        </div>
+  `;
+}
+
 function displayAllCards(data) {
   const mainSection = document.getElementById("mainSection");
   mainSection.innerHTML = "";
@@ -74,7 +176,7 @@ function displayAllCards(data) {
     // Card toiri
     const creatDiv = document.createElement("div");
     creatDiv.innerHTML = `
-      <div class="card bg-white shadow-xl p-3 space-y-3 h-full ${cardTopBorder} w-full">
+      <div onclick="loadModal(${item.id})" class="card bg-white shadow-xl p-3 space-y-3 h-full ${cardTopBorder} w-full">
 
         <!-- images part -->
         <div class="imgs flex justify-between items-center">
